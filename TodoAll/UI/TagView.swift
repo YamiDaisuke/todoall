ae2e934ac8fe8bbbbeac38e8d8b9b018cbcaa726
Franklin Cruz <daisuke.sysoft@gmail.com>
2020-12-27 11:49:17 -0300

    Implement a tag view

---
+++ TodoAll/UI/TagView.swift
@@ -0,0 +1,41 @@
+//
+//  TagView.swift
+//  TodoAll
+//
+//  Created by Franklin Cruz on 26-12-20.
+//  Copyright © 2020 S.Y.Soft. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    @State var text = "A Tag"
+    @State var color = Color.random()
+    @State var image: Image?
+    
+    var action = { }
+    
+    var body: some View {
+        HStack {
+            Text(self.text)
+                .font(.caption)
+                .padding(5)
+            self.image
+                .padding(5)
+        }
+        .background(self.color)
+        .foregroundColor(self.color.contrastFontColor)
+        .cornerRadius(10)
+        .padding(2)
+    }
+}
+
+struct TagView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagView(image: Image(systemName: "minus.circle.fill"))
+            .previewLayout(.sizeThatFits)
+        
+        TagView()
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ TodoAll/Utils/ColorExtensions.swift
@@ -0,0 +1,36 @@
+//
+//  ColorExtensions.swift
+//  TodoAll
+//
+//  Created by Franklin Cruz on 26-12-20.
+//  Copyright © 2020 S.Y.Soft. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static func random() -> Color {
+        let red = Double.random(in: 0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(.sRGB, red: red, green: green, blue: blue, opacity: 1)
+    }
+    
+    var contrastFontColor: Color {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: nil)
+        return isLightColor(red: red, green: green, blue: blue) ? .black : .white
+    }
+    
+    private func isLightColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> Bool {
+        let lightRed = red > 0.65
+        let lightGreen = green > 0.65
+        let lightBlue = blue > 0.65
+
+        let lightness = [lightRed, lightGreen, lightBlue].reduce(0) { $1 ? $0 + 1 : $0 }
+        return lightness >= 2
+    }
+}
